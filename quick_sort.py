c6ec46e18b7114f5e5454d3cb6d539288f4b5166
Banvaree lal dangi <banvareelaldangi8@gmail.com>
2025-02-13 22:05:45 +0530

    quick_sort.py

---
+++ quick_sort.py
@@ -0,0 +1,12 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+arr = [39, 12, 18, 85, 72, 10, 2, 18]
+sorted_arr = quick_sort(arr)
+print(sorted_arr)
